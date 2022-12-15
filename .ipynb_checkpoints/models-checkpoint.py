9f8597ae08dca6c539531dea6a2d33a0d2650c3e
sfandres94 <andres.sanchez1994@gmail.com>
2022-12-15 12:32:28 +0100

    Models.py: ssl models added

---
+++ .ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,90 @@
+import torch
+from lightly.models.modules.heads import SimSiamPredictionHead,SimSiamProjectionHead
+from lightly.models.modules.heads import SimCLRProjectionHead
+from lightly.utils.debug import std_of_l2_normalized
+from lightly.loss import NTXentLoss,NegativeCosineSimilarity
+
+
+class SimSiam(torch.nn.Module):
+    """
+    SimSiam self-supervised learning model.
+
+    Attributes:
+        backbone: Architecture of the CNN model.
+        num_ftrs: Dimension of the embeddings.
+        proj_hidden_dim: Dimension of the output of the prediction and projection heads.
+        pred_hidden_dim: Dimension of the prediction head.
+        out_dim: Dimension of the output of the prediction and projection heads.
+    """
+    def __init__(self, backbone, num_ftrs, proj_hidden_dim,
+                 pred_hidden_dim, out_dim):
+        """Constructor of the class."""
+        # Inheritance.
+        super().__init__()
+
+        # Blackbone model.
+        self.backbone = backbone
+
+        # Projection head (lightly).
+        self.projection_head = SimSiamProjectionHead(
+            num_ftrs, proj_hidden_dim, out_dim
+        )
+
+        # Prediction head (lightly).
+        self.prediction_head = SimSiamPredictionHead(
+            out_dim, pred_hidden_dim, out_dim
+        )
+
+        # Loss criterion.
+        self.criterion = NegativeCosineSimilarity()
+
+    def forward(self, x):
+        """How your model runs from input to output."""
+        # Get representations.
+        f = self.backbone(x).flatten(start_dim=1)
+
+        # Get projections.
+        z = self.projection_head(f)
+
+        # Get predictions.
+        p = self.prediction_head(z)
+
+        # Stop gradient.
+        z = z.detach()
+
+        return z, p
+
+
+class SimCLRModel(torch.nn.Module):
+    """
+    SimCLR self-supervised learning model.
+
+    Attributes:
+        backbone: Architecture of the CNN model.
+        hidden_dim: Dimension of the output of the projection head.
+    """
+    def __init__(self, backbone, hidden_dim):
+        """Constructor of the class."""
+        # Inheritance.
+        super().__init__()
+
+        # Blackbone model.
+        self.backbone = backbone
+
+        # Projection head.
+        self.projection_head = SimCLRProjectionHead(
+            hidden_dim, hidden_dim, 128
+        )
+
+        # Loss criterion.
+        self.criterion = NTXentLoss()
+
+    def forward(self, x):
+        """How your model runs from input to output."""
+        # Get representations.
+        h = self.backbone(x).flatten(start_dim=1)
+
+        # Get projections.
+        z = self.projection_head(h)
+
+        return z
