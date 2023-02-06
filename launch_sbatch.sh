35c0fce858e5033117a4bf014192464a637d0e51
sfandres94 <andres.sanchez1994@gmail.com>
2023-02-06 10:02:34 +0100

    Launch bash file modified and working

--- launch_sbatch.sh
+++ launch_sbatch.sh
@@ -5,29 +5,36 @@
 #SBATCH --nodes=1                                   ## Number of nodes.
 #SBATCH --partition=volta                           ## Request specific partition.
 #SBATCH --wait-all-nodes=1                          ## Controls when the execution begins.
-#SBATCH --time=00:30:00                             ## Job duration (Sergio: 24:00:00).
+#SBATCH --time=24:00:00                             ## Job duration (Sergio: 24:00:00).
 #SBATCH --gpus-per-node=4                           ## Number of GPUs on each node (Sergio: 4).
 #SBATCH --job-name=uexssl_%A_%a                     ## Name of the job.
 #SBATCH --output=uexssl_%A_%a.out                   ## Output file.
-#SBATCH --mail-type=ALL				                ## Type of notification via email.
-#SBATCH --mail-user=sfandres@unex.es                ## User to receive the email notification.
+#SBATCH --mail-type=ALL				    ## (not working) Type of notification via email.
+#SBATCH --mail-user=sfandres@unex.es                ## (not working) User to receive the email notification.
 #SBATCH --array=0-2:1                               ## Run job arrays.
 
-# Array of models.
+## Catch Slurm environment variables.
+job_id=${SLURM_ARRAY_JOB_ID}
+task_id=${SLURM_ARRAY_TASK_ID}
+
+## Array of models.
 array_models=("simsiam" "simclr" "barlowtwins")
-model=${array_models[${SLURM_ARRAY_TASK_ID}]}
+model=${array_models[${task_id}]}
 
-## Send email when job begin.
-cat mail.txt | /usr/bin/mail -s "Sbatch job array_task_id=${SLURM_ARRAY_TASK_ID} model=${model} began" sfandres@unex.es
+## Create a string for email subject.
+email_info="job_id=${job_id} task_id=${task_id} ssl_model=${model}"
 
-## Load the Python module.
+## Send email when job begin (two options).
+## cat email_body.txt | /usr/bin/mail -s "Sbatch ${email_info} began" sfandres@unex.es
+echo " " | /usr/bin/mail -s "Sbatch ${email_info} began" sfandres@unex.es
+
+## Load the Python module (not necessary).
 ## module load cuda/11.0.1
 
 ## Load virtual environment.
 source ~/lulc/lulc-venv/bin/activate
 
 ## Execute the Python script and pass the arguments.
-## srun python3 script.py ${model}
 srun python3 03_1-PyTorch-Sentinel-2_SSL_pretraining.py \
 ${model} \
 --dataset Sentinel2GlobalLULC \
@@ -39,4 +46,4 @@ ${model} \
 --cluster True
 
 ## Send email when job ends.
-cat uexssl_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.out | /usr/bin/mail -s "Sbatch job array_task_id=${SLURM_ARRAY_TASK_ID} model=${model} ended" sfandres@unex.es
+cat uexssl_${job_id}_${task_id}.out | /usr/bin/mail -s "Sbatch ${email_info} ended" sfandres@unex.es
