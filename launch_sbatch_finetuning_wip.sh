9f66e1e7df71ed602b3213fa5ff44f5608aa9e6e
sfandres94 <andres.sanchez1994@gmail.com>
2023-03-13 12:08:38 +0100

    Add file launch_sbatch_finetuning_wip

---
+++ launch_sbatch_finetuning_wip.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+## Shebang.
+
+## Resource request.
+#SBATCH --nodes=1                                   ## Number of nodes.
+#SBATCH --partition=volta                           ## Request specific partition.
+#SBATCH --wait-all-nodes=1                          ## Controls when the execution begins.
+#SBATCH --time=24:00:00                             ## Job duration (Sergio: 24:00:00).
+#SBATCH --gpus-per-node=2                           ## Number of GPUs on each node (Sergio: 4).
+#SBATCH --job-name=uexssl_%A_%a                     ## Name of the job.
+#SBATCH --output=uexssl_%A_%a.out                   ## Output file.
+#SBATCH --mail-type=ALL                             ## (not working) Type of notification via email.
+#SBATCH --mail-user=sfandres@unex.es                ## (not working) User to receive the email notification.
+#SBATCH --array=0-2:1                               ## Run job arrays.
+
+## Catch Slurm environment variables.
+job_id=${SLURM_ARRAY_JOB_ID}
+task_id=${SLURM_ARRAY_TASK_ID}
+
+## Array of models.
+array_models=("simsiam" "simclr" "barlowtwins")
+model=${array_models[${task_id}]}
+
+## Create a string for email subject.
+email_info="job_id=${job_id} task_id=${task_id} ssl_model=${model}"
+
+## Send email when job begin (two options).
+## cat email_body.txt | /usr/bin/mail -s "Sbatch ${email_info} began" sfandres@unex.es
+echo " " | /usr/bin/mail -s "Sbatch ${email_info} began" sfandres@unex.es
+
+## Load the Python module (not necessary).
+## module load cuda/11.0.1
+
+## Load virtual environment.
+## source ~/lulc/lulc-venv/bin/activate
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate lulc-conda
+
+## Execute the Python script and pass the arguments.
+srun python3 03_1-PyTorch-Sentinel-2_SSL_pretraining.py \
+${model} \
+--dataset Sentinel2GlobalLULC \
+--balanced_dataset False \
+--epochs 25 \
+--batch_size 128 \
+--ini_weights random \
+--show_fig False \
+--cluster True
+## srun python3 testing_GPU_PyTorch.py
+
+## Send email when job ends.
+## cat uexssl_${job_id}_${task_id}.out | /usr/bin/mail -s "Sbatch ${email_info} ended" sfandres@unex.es
+/usr/bin/mail -a uexssl_${job_id}_${task_id}.out -s "Sbatch ${email_info} ended" sfandres@unex.es
