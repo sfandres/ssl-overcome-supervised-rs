72790a2dc3eac0f51efe578f1990fe1e82deaae1
sfandres94 <andres.sanchez1994@gmail.com>
2023-03-15 11:15:52 +0100

    Fix bugs in dataset_split. Improved with new options

--- scripts/dataset_split.py
+++ scripts/dataset_split.py
@@ -9,12 +9,14 @@ Author:
 
 
 import sys
+import os
+import argparse
+import ast
+import splitfolders
+
+
 sys.path.append("../")
 from utils.computation import Experiment
-import splitfolders
-import argparse
-import sys
-import os
 
 
 def create_arg_parser():
@@ -27,22 +29,40 @@ def create_arg_parser():
     )
 
     parser.add_argument(
-        'input_dir',
+        'input_path',
         type=str,
-        help='Path to the input directory where the raw dataset is stored.'
+        help='path to the input directory where the raw dataset is stored (one class per folder).'
     )
 
     parser.add_argument(
-        'output_dir',
+        'output_path',
         type=str,
-        help='Path to the output directory where the new datasets will be stored.'
+        help='path to the output directory where the new datasets will be stored.'
     )
 
     parser.add_argument(
         '--output_ini_name',
+        '-n',
         type=str,
         default='NewDataset',
-        help="Beginning of each folder's name (default = NewDataset)."
+        help="beginning of each folder's name (default = 'NewDataset')."
+    )
+
+    parser.add_argument(
+        '--train_ratios',
+        '-trs',
+        nargs='+',
+        type=str,
+        default='[.9]',
+        help="train ratios of the splits per unit separated by spaces (example: .9 .8 .7)."
+    )
+
+    parser.add_argument(
+        '--val_ratio',
+        '-vr',
+        type=float,
+        default=.25,
+        help="val ratio of the splits per unit (default = 0.25)."
     )
 
     return parser
@@ -56,34 +76,43 @@ if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
 
 # Show info.
-initial_dir_dataset = args.input_dir
-print(f'\nPath to the input raw data: {initial_dir_dataset}')
+initial_dir_dataset = args.input_path
+print(f'\nPath to the input raw data:\t{initial_dir_dataset}')
 
-output_dir_datasets = args.output_dir
-print(f'\nPath to the output split data: {output_dir_datasets}')
+output_dir_datasets = args.output_path
+print(f'Path to the output split data:\t{output_dir_datasets}')
 
 target_dataset_name = args.output_ini_name
-print(f'\nOutput folder name: {target_dataset_name}')
+print(f'Output folder name:\t\t{target_dataset_name}')
 
 # Experiment class for reproducibility.
 exp = Experiment()
 exp.reproducibility()
-print(f'\nSeed used: {exp.seed}')
+print(f'\nSeed used:\t   {exp.seed}')
 
 # Target division according to the train dataset.
-train = [.98, .9, .7, .5, .3, .1, .02]
-ratios = []
+# Get train ratios from command line arguments.
+train_ratios = args.train_ratios
+if isinstance(train_ratios, list):
+    train_ratios = [float(num) for num in train_ratios]
+else:
+    train_ratios = [float(num) for num in ast.literal_eval(train_ratios)]
+print(f'Train ratios:\t   {train_ratios}')
+
+val_ratio = args.val_ratio
+print(f'Validation ratio:  {val_ratio}\n')
 
 # Iterate over the train splits.
-for i, train_s in enumerate(train):
+ratios = []
+for i, train_s in enumerate(train_ratios):
 
-    # Rest: 30% validation and 70% test.
-    val_s = round((1 - train_s) * .3, 4)
+    # Rest: 25% validation and 75% test.
+    val_s = round((1 - train_s) * val_ratio, 4)
     test_s = round(1 - train_s - val_s, 4)
 
     # Stats.
     sum_s = train_s + val_s + test_s
-    print(f'{i}- train: {train_s:.3f}   '
+    print(f'[{i}] --> train: {train_s:.3f}   '
           f'val: {val_s:.4f}   '
           f'test: {test_s:.4f}   '
           f'sum: {sum_s}')
@@ -94,7 +123,7 @@ for i, train_s in enumerate(train):
                    test_s))
 
 # Print the list of ratios to be used.
-print(f'{ratios}\n')
+print(f'{ratios}')
 
 # Split with a ratio.
 # To only split into training and validation set,
@@ -112,7 +141,7 @@ for ratio in ratios:
                     f'-ratio={str_ratio}'
                     f'-seed={exp.seed}')
 
-    print(f'Building dataset: {dataset_name}...')
+    print(f'\nBuilding dataset: {dataset_name}...')
 
     # Split the dataset (default values).
     splitfolders.ratio(initial_dir_dataset,
@@ -123,7 +152,7 @@ for ratio in ratios:
                        group_prefix=None,
                        move=False)
 
-    print('Successfully created!\n')
+    print('Successfully created!')
 
 # Balanced dataset.
 # Split val/test with a fixed number of items, e.g. `(100, 100)`, for each set.
