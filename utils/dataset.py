ab9cb14595178e5d65652bbf6d9e43dbfbda5dc9
sfandres94 <andres.sanchez1994@gmail.com>
2023-03-16 10:34:42 +0100

    Add GaussianBlur class to datasets

--- utils/dataset.py
+++ utils/dataset.py
@@ -363,3 +363,15 @@ def show_one_batch(axes, num_cols, dataloader, idx_to_class, batch_id=0,
                 out = ax.imshow(image, **param_dict)
 
             return out
+
+
+class GaussianBlur(object):
+    """Gaussian blur augmentation in SimCLR https://arxiv.org/abs/2002.05709"""
+
+    def __init__(self, sigma=[.1, 2.]):
+        self.sigma = sigma
+
+    def __call__(self, x):
+        sigma = random.uniform(self.sigma[0], self.sigma[1])
+        x = x.filter(ImageFilter.GaussianBlur(radius=sigma))
+        return x
