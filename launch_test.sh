fad636c94f7fda453bcdfb1608480324582c615b
sfandres94 <andres.sanchez1994@gmail.com>
2023-03-27 21:00:07 +0200

    Update launch bash

--- launch_test.sh
+++ launch_test.sh
@@ -8,9 +8,9 @@ model=${array_models[1]}
 ## Execute the Python script and pass the arguments.
 python3 03_1-PyTorch-Sentinel-2_SSL_pretraining.py \
 ${model} \
---dataset_name "Sentinel2GlobalLULC_SSL" \
---dataset_ratio "(0.400,0.1500,0.4500)" \
---epochs "2" \
---batch_size "64" \
---ini_weights "random" \
+--dataset_name=Sentinel2GlobalLULC_SSL \
+--dataset_ratio=\(0.020,0.0196,0.9604\) \
+--epochs=25 \
+--batch_size=64 \
+--ini_weights=random \
 --cluster
