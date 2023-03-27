ce0ddc960c6f0700dd70bab6410e3bbb5a5b0397
sfandres94 <andres.sanchez1994@gmail.com>
2023-03-27 14:43:31 +0200

    Add new bash script to launch the experiments locally

---
+++ launch_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## Shebang.
+
+## Array of models.
+array_models=("simsiam" "simclr" "barlowtwins")
+model=${array_models[1]}
+
+## Execute the Python script and pass the arguments.
+python3 03_1-PyTorch-Sentinel-2_SSL_pretraining.py \
+${model} \
+--dataset_name "Sentinel2GlobalLULC_SSL" \
+--dataset_ratio "(0.400,0.1500,0.4500)" \
+--epochs "2" \
+--batch_size "64" \
+--ini_weights "random" \
+--cluster
