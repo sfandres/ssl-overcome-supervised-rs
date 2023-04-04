e547fa08a1d98fee70a3c7f2794b9bdbebb9d38a
sfandres94 <andres.sanchez1994@gmail.com>
2023-04-04 13:03:17 +0200

    Improve the simclr module

---
+++ utils/simclr.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This module provides the SimCLR class.
+
+https://paperswithcode.com/method/simclr
+
+Author: Andres J. Sanchez-Fernandez
+Email: sfandres@unex.es
+Date: 2023-04-04
+"""
+
+
+import torch
+import torch.nn as nn
+from lightly.models.modules.heads import SimCLRProjectionHead
+from lightly.loss import NTXentLoss
+from .base_model import BaseModel
+
+
+class SimCLR(BaseModel):
+    """
+    SimCLR self-supervised learning model.
+
+    Attributes:
+        backbone (nn.Module): a ResNet backbone network.
+        projection_head (nn.Sequential): a projection head network that generates feature embeddings.
+        criterion (NTXentLoss): Contrastive Cross Entropy Loss.
+
+    Methods:
+        forward(x): computes the forward pass of the SimCLR model.
+        training_step(batch): computes the loss of the current batch (tuple of tensors).
+    """
+
+    def __init__(
+        self,
+        backbone: nn.Sequential,
+        input_dim: int = 512,
+        hidden_dim: int = 512,
+        output_dim: int = 128
+    ):
+        """
+        Initializes a new SimCLR model.
+
+        Args:
+            backbone (nn.Sequential): a ResNet backbone network.
+            input_dim (int): the number of input features to the fc layer.
+            hidden_dim (int): the dimension of the hidden layers.
+            output_dim (int): the dimensionality of the feature embeddings
+            produced by the projection head network.
+        """
+
+        # Call the BaseModel superclass constructor.
+        super().__init__()
+
+        # Blackbone model.
+        self.backbone = backbone
+
+        # Projection head.
+        self.projection_head = SimCLRProjectionHead(input_dim=input_dim,
+                                                    hidden_dim=hidden_dim,
+                                                    output_dim=output_dim)
+
+        # Loss criterion (memory bank = 1 for MoCo).
+        self.criterion = NTXentLoss(temperature=0.5, memory_bank_size=0)
+
+    def forward(
+        self,
+        x: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Computes the forward pass of the SimCLR model.
+
+        Args:
+            x (torch.Tensor): a batch of input images.
+
+        Returns:
+            torch.Tensor: a tensor of feature embeddings produced by the projection head network.
+        """
+
+        # Feature extraction using the ResNet backbone.
+        z = self.backbone(x).flatten(start_dim=1)
+
+        # Feature embeddings.
+        h = self.projection_head(z)
+
+        return h
+
+    def training_step(
+        self,
+        two_batches: tuple[torch.Tensor, torch.Tensor],
+    ) -> float:
+        """
+        Performs a single training step on a batch of transformed images.
+
+        Args:
+            two_batches (tuple): a tuple of two batches of transformed images,
+            where each batch is a tensor of size (batch_size, C, H, W).
+
+        Returns:
+            float: The loss value for the current batch.
+        """
+
+        # Two batches of transformed images.
+        x0, x1 = two_batches
+
+        # Output projections of both transformed batches.
+        z0 = self.forward(x0)
+        z1 = self.forward(x1)
+
+        # Contrastive Cross Entropy Loss.
+        loss = self.criterion(z0, z1)
+
+        return loss
