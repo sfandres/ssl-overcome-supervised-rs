4dc397219ec2ed48bf4131437bea5a9f9336a786
sfandres94 <andres.sanchez1994@gmail.com>
2023-04-04 12:50:58 +0200

    Update launch_test.sh with new model names

--- launch_test.sh
+++ launch_test.sh
@@ -2,8 +2,9 @@
 ## Shebang.
 
 ## Array of models.
-array_models=("simsiam" "simclr" "barlowtwins")
-model=${array_models[1]}
+array_models=("SimSiam" "SimCLR" "SimCLRv2" "BarlowTwins")
+idx = 0
+model=${array_models[idx]}
 
 ## Execute the Python script and pass the arguments.
 python3 03_1-PyTorch-Sentinel-2_SSL_pretraining.py \
@@ -11,7 +12,8 @@ ${model} \
 --backbone_name=resnet18 \
 --dataset_name=Sentinel2GlobalLULC_SSL \
 --dataset_ratio=\(0.400,0.1500,0.4500\) \
---epochs=100 \
+--epochs=500 \
 --batch_size=64 \
 --ini_weights=random \
---cluster
+--cluster \
+## '--resume_training'
