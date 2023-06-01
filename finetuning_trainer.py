e01b964353c94491de4ba12e9ff074f49409946e
sfandres94 <andres.sanchez1994@gmail.com>
2023-06-01 13:08:17 +0200

    Improve csv name with lr

--- finetuning_trainer.py
+++ finetuning_trainer.py
@@ -173,7 +173,7 @@ class Trainer:
 
                 csv_file = os.path.join(
                     os.getcwd(),
-                    f'{args.task_name}_pctrain_{args.dataset_train_pc:.3f}_{args.model_name}.csv'
+                    f'{args.task_name}_pctrain_{args.dataset_train_pc:.3f}_lr_{args.learning_rate}_{args.model_name}.csv'
                 )
 
                 if epoch == 0:
