47c0ac6fcf4226accb7c2f2ff6fae67a8c5bc779
sfandres94 <andres.sanchez1994@gmail.com>
2023-06-04 00:38:53 +0200

    Update launch script with new arguments

--- finetuning_run_localhost.sh
+++ finetuning_run_localhost.sh
@@ -2,10 +2,10 @@
 
 # Define the variables.
 downstream=("multiclass" "multilabel")
-train_rates=(".05" ".1" ".25" "1.")
-learning_rates=("0.001" "0.01" "0.1")
+train_rates=("0.05" "0.1" "0.25" "0.5")  # "1.0"
+learning_rates=("0.025" "0.05" "0.1")
 models=("Random" "Imagenet" "BarlowTwins" "MoCov2" "SimCLR" "SimCLRv2" "SimSiam")
-backbones=("resnet18")  # "resnet50")
+backbones=("resnet50" "resnet18")
 
 # Loop over the sbatch commands.
 for d in "${downstream[@]}"; do
@@ -13,8 +13,8 @@ for d in "${downstream[@]}"; do
         for lr in "${learning_rates[@]}"; do
             for m in "${models[@]}"; do
                 for b in "${backbones[@]}"; do
-                    echo torchrun finetuning.py $m $d -nw 4 -bs 64 -e 50 -lr $lr --dataset_train_pc=$tr
-                    torchrun finetuning.py $m $d -nw 4 -bs 64 -e 50 -lr $lr --dataset_train_pc=$tr
+                    echo torchrun finetuning.py $m $d -bn $b -tr $tr -e 50 -lr $lr -bs 64 -nw 4
+                    torchrun finetuning.py $m $d -bn $b -tr $tr -e 50 -lr $lr -bs 64 -nw 4
                 done
             done
         done
