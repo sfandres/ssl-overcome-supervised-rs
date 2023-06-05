58fc978d9f3087fe4ee91d4a3ce292334d82c1be
sfandres94 <andres.sanchez1994@gmail.com>
2023-06-05 13:00:24 +0200

    Ease coding

--- finetuning_run_localhost.sh
+++ finetuning_run_localhost.sh
@@ -2,10 +2,10 @@
 
 # Define the variables.
 downstream=("multiclass" "multilabel")
-train_rates=("0.05" "0.1" "0.25" "0.5")  # "1.0"
+train_rates=("0.05" "0.1" "0.25")  # "0.5" "1.0"
 learning_rates=("0.025" "0.05" "0.1")
 models=("Random" "Imagenet" "BarlowTwins" "MoCov2" "SimCLR" "SimCLRv2" "SimSiam")
-backbones=("resnet50" "resnet18")
+backbones=("resnet18" "resnet50")
 
 # Loop over the sbatch commands.
 for b in "${backbones[@]}"; do
@@ -13,8 +13,8 @@ for b in "${backbones[@]}"; do
         for d in "${downstream[@]}"; do
             for lr in "${learning_rates[@]}"; do
                 for m in "${models[@]}"; do
-                    echo torchrun finetuning.py $m $d -bn $b -tr $tr -e 50 -lr $lr -bs 64 -nw 4
-                    torchrun finetuning.py $m $d -bn $b -tr $tr -e 50 -lr $lr -bs 64 -nw 4
+                    command="torchrun finetuning.py $m $d -bn $b -tr $tr -e 50 -lr $lr -bs 64 -nw 4"
+                    echo $command; $command
                 done
             done
         done
