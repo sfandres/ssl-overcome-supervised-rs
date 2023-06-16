d1b5d7ecd1af6d0cf50108e45eb5e11a6edcc7d4
sfandres94 <andres.sanchez1994@gmail.com>
2023-06-16 11:04:27 +0200

    Add type of experiment to loop

--- pytorch-DDP-slurm_launch_loop.sh
+++ pytorch-DDP-slurm_launch_loop.sh
@@ -2,61 +2,45 @@
 
 # Help function.
 function display_help {
-    echo "Usage: $0 NODE"
+    echo "Usage: $0 EXPERIMENT"
     echo "Arguments:"
-    echo "  NODE          Starting target node (a number between 1 and 75)"
+    echo "  EXPERIMENT    Type of experiment to carry out ('RayTune', 'DDP', or 'Balanced')"
     echo "  -h, --help    Display this help message"
     exit 0
 }
 
-# Function to include a leading 0 if num<10.
-format_number() {
-    local num=$1
-    local formatted_num
-
-    # Check if num is less than 10
-    if ((num < 10)); then
-        # Add a leading zero to the number and store it in a string
-        formatted_num="0$num"
-    else
-        # Number is 10 or greater, no leading zero required
-        formatted_num="$num"
-    fi
-
-    echo "$formatted_num"
-}
-
-# Parse arguments.
-node=$1
-echo "Starting node (4-GPUs): $node"
+# Parse and check the first argument.
+experiment=$1
 
 if [[ $1 == "-h" || $1 == "--help" ]]; then
     display_help
 
-elif [[ -z $node ]]; then
-    echo "Error: Starting node is required."
+elif [[ -z $experiment ]]; then
+    echo "Error: Selecting a type of experiment is required."
     display_help
-elif [[ ! $node -ge 1 || ! $node -le 75 ]]; then
-    echo "Error: Not a number or a value in the rage 1-75."
+
+elif [[ $experiment != "RayTune" && $experiment != "DDP" && $experiment != "Balanced" ]]; then
+    echo "Error: Invalid type of experiment. Supported ones are 'RayTune', 'DDP', and 'Balanced'."
     display_help
+
+else
+    echo "Experiment: $experiment"
 fi
 
 # Define the variables.
 models=("BarlowTwins" "MoCov2" "SimCLR" "SimCLRv2" "SimSiam")
-backbones=("resnet18" "resnet50")
+backbones=("resnet18")  # "resnet50"
 
 # Loop over the sbatch commands.
 for m in "${models[@]}"; do
     for b in "${backbones[@]}"; do
 
-        # Range of target nodes.
-        target_nodes="dp-esb[$(format_number $node)-$(format_number $(($node + 3)))]"
-
         # Run.
-        echo sbatch -p dp-esb -w $target_nodes -t 1080 -J ${m}_${b} -o out_${m}_${b}.out pytorch-DDP-slurm.sh ${m} ${b}
-        sbatch -p dp-esb -w $target_nodes -t 1080 -J ${m}_${b} -o out_${m}_${b}.out pytorch-DDP-slurm.sh ${m} ${b}
+        command="sbatch -J ${m}_${b} -o out_${m}_${b}.out pytorch-DDP-slurm.sh ${m} ${b} ${experiment}"
+        echo ${command}
+        ${command}
 
-        # Check if continue (drain/alloc nodes in between).
+        # Check if continue.
         read -p "Continue? (yes/enter to continue, no to exit): " input
         while [[ -n $input && $input != "yes" && $input != "no" ]]; do
             read -p "Invalid input. Please type 'yes'/enter or 'no': " input
@@ -65,8 +49,6 @@ for m in "${models[@]}"; do
             break
         fi
 
-        # Increment the starting node by 4.
-        ((node += 4))
     done
 
     if [[ $input == "no" ]]; then
