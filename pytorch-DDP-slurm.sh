8ea2ec4342aad548cb0ee76e5922cd4bebd44440
sfandres94 <andres.sanchez1994@gmail.com>
2023-06-23 11:38:53 +0200

    Change time limit to 24h

--- pytorch-DDP-slurm.sh
+++ pytorch-DDP-slurm.sh
@@ -16,7 +16,7 @@
 #--------------------------------------------
 # Common options.
 #SBATCH --partition=volta                           # Request specific partition.
-#SBATCH --time=48:00:00                             # Job duration (72h is the limit).
+#SBATCH --time=24:00:00                             # Job duration (72h is the limit).
 #SBATCH --cpus-per-task=4                           # Number of cpu-cores per task (>1 if multi-threaded tasks).
 #SBATCH --nodes=1                                   # Number of nodes.
 
