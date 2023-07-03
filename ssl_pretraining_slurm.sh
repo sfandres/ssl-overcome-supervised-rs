e9de849c5f6a35d5d00395f2f5fbbeceea255f77
sfandres94 <andres.sanchez1994@gmail.com>
2023-07-03 14:13:27 +0200

    Try with 2-GPUs nodes

--- ssl_pretraining_slurm.sh
+++ ssl_pretraining_slurm.sh
@@ -17,7 +17,7 @@
 # Specific options.
 #--------------------------------------------
 #SBATCH --ntasks=1                                  # Number of tasks.
-#SBATCH --gpus-per-node=4                           # Min. number of GPUs on each node.
+#SBATCH --gpus-per-node=2                           # Min. number of GPUs on each node.
 #SBATCH --mem=200G                                  # Real memory required per node.
 # #SBATCH --exclusive                                 # The job can not share nodes with other running jobs.
 #--------------------------------------------
