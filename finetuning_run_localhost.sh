b388915e3e9919d5f42aa7774ec3a6f567e87abe
sfandres94 <andres.sanchez1994@gmail.com>
2023-07-04 16:30:51 +0200

    Carry out many changes

--- finetuning_run_localhost.sh
+++ finetuning_run_localhost.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Define the variables.
-backbones=("resnet18")                          # "resnet18" "resnet50"
-train_rates=("0.01" "0.05" "0.1")               # "0.01" "0.05" "0.1" "0.25" "0.5" "1.0"
-downstream=("multiclass" "multilabel")          # "multiclass" "multilabel"
-learning_rates=("0.01")                         # Not used for Ray Tune; then 10 times smaller if LP+FT is enabled
-models=("Supervised")                           # "BarlowTwins" "MoCov2" "SimCLR" "SimCLRv2" "SimSiam"
-ini_weights=("random" "imagenet")               # "random" "imagenet"
-balanced_dataset=(" " "-bd")                    # " " "-bd"
-batch_size=64
-num_workers=2                                   # 4
-transfer_learning=("LP" "FT")                   # "LP" "FT" "LP+FT"
-seed=42
+backbones=("resnet18")                                  # "resnet18" "resnet50"
+train_rates=("0.25" "0.5" "1.0")                        # "0.01" "0.05" "0.1" "0.25" "0.5" "1.0"
+downstream=("multiclass" "multilabel")                  # "multiclass" "multilabel"
+learning_rates=("0.01")                                 # Not used for Ray Tune; then 10 times smaller if LP+FT is enabled
+models=("Supervised")                                   # "BarlowTwins" "MoCov2" "SimCLR" "SimCLRv2" "SimSiam"
+ini_weights=("random" "imagenet")                       # "random" "imagenet"
+balanced_dataset=(" " "-bd")                            # " " "-bd"
 epochs=12
+batch_size=64
+num_workers=2                                           # 4
+transfer_learning="FT"                                  # "LP" "FT" "LP+FT"
+seed=42
 # more_options=""
+# more_options="--load_best_hyperparameters"
 more_options="--ray_tune=gridsearch --grace_period=4 --num_samples_trials=3 --gpus_per_trial=1"
 
 # Troubleshooting.
