0cb5501e83dbf8f5163f718a2c68f8781d49d346
sfandres94 <andres.sanchez1994@gmail.com>
2023-07-04 16:48:00 +0200

    Fix bug

--- ssl_pretraining_slurm.sh
+++ ssl_pretraining_slurm.sh
@@ -9,7 +9,7 @@
 #--------------------------------------------
 #SBATCH --partition=volta                           # Request specific partition.
 #SBATCH --time=24:00:00                             # Job duration (72h is the limit).
-#SBATCH --cpus-per-task=2                           # Number of cpu-cores per task (>1 if multi-threaded tasks).
+#SBATCH --cpus-per-task=4                           # Number of cpu-cores per task (>1 if multi-threaded tasks).
 #SBATCH --nodes=1                                   # Number of nodes.
 #SBATCH --mail-type=ALL                             # Type of notification via email.
 #SBATCH --mail-user=sfandres@unex.es                # User to receive the email notification.
@@ -17,7 +17,7 @@
 # Specific options.
 #--------------------------------------------
 #SBATCH --ntasks=1                                  # Number of tasks.
-#SBATCH --gpus-per-node=2                           # Min. number of GPUs on each node.
+#SBATCH --gpus-per-node=4                           # Min. number of GPUs on each node.
 # #SBATCH --mem=200G                                  # Real memory required per node.
 # #SBATCH --exclusive                                 # The job can not share nodes with other running jobs.
 #--------------------------------------------
