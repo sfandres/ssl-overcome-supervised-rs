f8b67c903f2d080a0870b61f93a0229400601124
sfandres94 <andres.sanchez1994@gmail.com>
2023-07-07 14:08:33 +0200

    Update parameters

--- finetuning_run_localhost.sh
+++ finetuning_run_localhost.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Define the variables.
-backbones=("resnet18")                                  # "resnet18" "resnet50"
-train_rates=("0.01" "0.05" "0.1" "0.25" "0.5" "1.0")    # "0.01" "0.05" "0.1" "0.25" "0.5" "1.0"
-downstream=("multiclass" "multilabel")                  # "multiclass" "multilabel"
-learning_rates=("0.01")                                 # Not used for Ray Tune or --load_best_hyperparameters
-models=("Supervised")                                   # "BarlowTwins" "MoCov2" "SimCLR" "SimCLRv2" "SimSiam"
-ini_weights=("random" "imagenet")                       # "random" "imagenet"
-balanced_dataset=(" " "-bd")                            # " " "-bd"
-transfer_learning=("LP" "FT" "LP+FT")                   # "LP" "FT" "LP+FT"
-epochs=12
+backbones=("resnet18")                                      # "resnet18" "resnet50"
+train_rates=("0.01") # "0.05" "0.1" "0.25" "0.5" "1.0")     # "0.01" "0.05" "0.1" "0.25" "0.5" "1.0"
+downstream=("multiclass" "multilabel")                      # "multiclass" "multilabel"
+models=("Supervised")                                       # "Supervised" "BarlowTwins" "MoCov2" "SimCLR" "SimCLRv2" "SimSiam"
+ini_weights=("random" "imagenet")                           # "random" "imagenet"
+balanced_dataset=(" " "-bd")                                # " " "-bd"
+transfer_learning=("LP" "FT" "LP+FT")                       # "LP" "FT" "LP+FT"
+seeds=("42" "5" "97")
+epochs=100                                                  # 12 for Ray Tune
+learning_rate=0.01                                          # Not used for Ray Tune or --load_best_hyperparameters
+save_every=5
 batch_size=64
-num_workers=2                                           # 4
-seed=42
+num_workers=2
 # more_options=""
 # more_options="--ray_tune=gridsearch --grace_period=4 --num_samples_trials=3 --gpus_per_trial=1"
 more_options="--load_best_hyperparameters"
@@ -26,18 +27,18 @@ export RAY_PICKLE_VERBOSE_DEBUG=1
 for b in "${backbones[@]}"; do
     for tr in "${train_rates[@]}"; do
         for d in "${downstream[@]}"; do
-            for lr in "${learning_rates[@]}"; do
-                for tl in "${transfer_learning[@]}"; do
+            for tl in "${transfer_learning[@]}"; do
+                for s in "${seeds[@]}"; do
                     for m in "${models[@]}"; do
                         # Not considering balanced_dataset.
                         if [ "$m" = "Supervised" ]; then
                             for iw in "${ini_weights[@]}"; do
-                                command="torchrun finetuning.py $m $d -bn $b -tr $tr -e $epochs -lr $lr -bs $batch_size -nw $num_workers -iw $iw -tl $tl -s $seed $more_options"
+                                command="torchrun finetuning.py $m $d -bn $b -tr $tr -e $epochs -lr $learning_rate -se $save_every -bs $batch_size -nw $num_workers -iw $iw -tl $tl -s $s $more_options"
                                 echo $command; $command >> out_finetuning_$d.out
                             done
                         else
                             for bd in "${balanced_dataset[@]}"; do
-                                command="torchrun finetuning.py $m $d -bn $b -tr $tr -e $epochs -lr $lr -bs $batch_size -nw $num_workers -iw random -tl $tl -s $seed $bd $more_options"
+                                command="torchrun finetuning.py $m $d -bn $b -tr $tr -e $epochs -lr $learning_rate -se $save_every -bs $batch_size -nw $num_workers -iw random -tl $tl -s $s $bd $more_options"
                                 echo $command; $command >> out_finetuning_$d.out
                             done
                         fi
