6e20823755a49654ae5be31d7d275318cc5baefe
sfandres94 <andres.sanchez1994@gmail.com>
2023-07-10 13:08:24 +0200

    Work in progress 2

--- scripts/plot_final_graphs.py
+++ scripts/plot_final_graphs.py
@@ -67,6 +67,11 @@ def main(args):
     transfer_learning_algs = ['_tl=LP_', '_tl=FT_', '_tl=LP+FT_']
     print(f"{'TL algorithms:'.ljust(16)}{transfer_learning_algs}") if args.verbose else None
 
+    # Set the models.
+    # models = ['SSL', 'Supervised-ImageNet', 'Supervised-random']
+    models = ['Supervised-ImageNet', 'Supervised-random']
+    print(f"{'Models:'.ljust(16)}{models}") if args.verbose else None
+
     # Iterate over the algorithms.
     for tla in transfer_learning_algs:
 
@@ -85,48 +90,55 @@ def main(args):
             # Get a list of all directories.
             files = os.listdir(curr_path)
             filtered_files = sorted([f for f in files if tla in f])
-            print(f"{'* Target files:'}\n{filtered_files}") if args.verbose else None
+
+            # Filter the files to include only the ones with the desired pattern.
+            mean_files = [f for f in filtered_files if 'pp_mean_' in f]
+            std_files = [f for f in filtered_files if 'pp_std_' in f]
+            # print(f"{'* Target files:'}\n{mean_files}\n{std_files}") if args.verbose else None
+
+            # Iterate over the models.
+            for i, model in enumerate(models):
+
+                # Create the last filters according to the target model.
+                if model == 'SSL':
+                    filter1 = 'BarlowTwins'
+                    filter2 = '_iw=random'
+                elif model == 'Supervised-ImageNet':
+                    filter1 = 'Supervised'
+                    filter2 = '_iw=imagenet'
+                elif model == 'Supervised-random':
+                    filter1 = 'Supervised'
+                    filter2 = '_iw=random'
+
+                # Filter current files.
+                curr_mean = [f for f in mean_files
+                             if ('pp_mean_' in f and filter1 in f and filter2 in f)][0]
+                curr_std = [f for f in std_files
+                            if ('pp_std_' in f and filter1 in f and filter2 in f)][0]
+
+                curr_mean_filename = os.path.join(curr_path, curr_mean)
+                curr_std_filename = os.path.join(curr_path, curr_std)
+
+                if args.verbose:
+                    print(f"\n{'- File:'.ljust(9)}{curr_mean_filename}")
+                    print(f"{'- File:'.ljust(9)}{curr_std_filename}")
 
 
+            # # Read the CSV file into a pandas DataFrame.
+            # filename = os.path.join(curr_path, mean_files[0])
+            # print(f'\nfilename: {filename}')
+            # res_last_epoch = pd.read_csv(filename).iloc[-1, :]
+            # print(res_last_epoch)
 
 
+    # # Iterate over the different transfer learning algorithms.
+    # for transfer_learning in results:
 
-
-
-    # Filter the files to include only the ones with the desired pattern.
-    results = {}
-    lp_mean = sorted([f for f in files
-                      if ('pp_mean_' in f and '_tl=LP_' in f)])
-    lp_std = sorted([f for f in files
-                     if ('pp_std_' in f and '_tl=LP_' in f)])
-    ft_mean = sorted([f for f in files
-                      if ('pp_mean_' in f and '_tl=FT_' in f)])
-    ft_std = sorted([f for f in files
-                     if ('pp_std_' in f and '_tl=FT_' in f)])
-    lpft_mean = sorted([f for f in files
-                        if ('pp_mean_' in f and '_tl=LP+FT_' in f)])
-    lpft_std = sorted([f for f in files
-                       if ('pp_std_' in f and '_tl=LP+FT_' in f)])
-    results['lp'] = {'mean': lp_mean, 'std': lp_std}
-    # results['ft'] = {'mean': ft_mean, 'std': ft_std}
-    # results['lpft'] = {'mean': lpft_mean, 'std': lpft_std}
-
-    # Verbose.
-    if args.verbose:
-        print('\nFiles being loaded:')
-        for tl in results:
-            print(f'{tl}:')
-            for metric in results[tl]:
-                print(f"{f'* {metric}:'.ljust(8)} {results[tl][metric]}")
-
-    # Iterate over the different transfer learning algorithms.
-    for transfer_learning in results:
-
-        # Read the CSV file into a pandas DataFrame.
-        filename = os.path.join(args.input, results[transfer_learning]['mean'][0])
-        print(f'\nfilename: {filename}')
-        res_last_epoch = pd.read_csv(filename).iloc[-1, :]
-        print(res_last_epoch)
+    #     # Read the CSV file into a pandas DataFrame.
+    #     filename = os.path.join(args.input, results[transfer_learning]['mean'][0])
+    #     print(f'\nfilename: {filename}')
+    #     res_last_epoch = pd.read_csv(filename).iloc[-1, :]
+    #     print(res_last_epoch)
 
     return 0
 
