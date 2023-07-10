892b848071df705b3ca180de001928641245a26d
sfandres94 <andres.sanchez1994@gmail.com>
2023-07-10 13:43:33 +0200

    Fix links to files

--- scripts/plot_final_graphs.py
+++ scripts/plot_final_graphs.py
@@ -72,72 +72,90 @@ def main(args):
     models = ['Supervised-ImageNet', 'Supervised-random']
     print(f"{'Models:'.ljust(16)}{models}") if args.verbose else None
 
-    # Create a dictionary to add the found results.
-    results = {}
-
     # Iterate over the algorithms.
     for tla in transfer_learning_algs:
 
         # Show information.
         if args.verbose:
             print(f"\n---------------------------------------------------") 
-            print(f"{'Current TL:'.ljust(13)}{tla}")
+            print(f"{'Curr TL:'.ljust(13)}{tla}")
 
-        # Initialize dict.
-        results[tla] = {}
-        print(results)
+        # Iterate over the models.
+        for model in models:
 
-        # Iterate over the dirs.
-        for dir in filtered_dirs:
+            # Create the last filters according to the target model.
+            if model == 'SSL':
+                filter1 = 'BarlowTwins'
+                filter2 = '_iw=random'
+            elif model == 'Supervised-ImageNet':
+                filter1 = 'Supervised'
+                filter2 = '_iw=imagenet'
+            elif model == 'Supervised-random':
+                filter1 = 'Supervised'
+                filter2 = '_iw=random'
+            mean_files, std_files = [], []
+            mean_values, std_values = [], []
+            print(f"\n{'Curr model:'.ljust(13)}{model}")
 
-            # Build current path.
-            curr_path = os.path.join(args.input, dir)
-            print(f"\n{'* Path:'.ljust(9)}{curr_path}") if args.verbose else None
+            # Iterate over the dirs.
+            for ratio in filtered_dirs:
 
-            # Get a list of all directories.
-            files = os.listdir(curr_path)
-            filtered_files = sorted([f for f in files if tla in f])
+                # Build current path.
+                curr_path = os.path.join(args.input, ratio)
+                # print(f"\n{'* Path:'.ljust(9)}{curr_path}") if args.verbose else None
 
-            # Filter the files to include only the ones with the desired pattern.
-            mean_files = [f for f in filtered_files if 'pp_mean_' in f]
-            std_files = [f for f in filtered_files if 'pp_std_' in f]
-            # print(f"{'* Target files:'}\n{mean_files}\n{std_files}") if args.verbose else None
-
-            # Iterate over the models.
-            for model in models:
-
-                # Create the last filters according to the target model.
-                if model == 'SSL':
-                    filter1 = 'BarlowTwins'
-                    filter2 = '_iw=random'
-                elif model == 'Supervised-ImageNet':
-                    filter1 = 'Supervised'
-                    filter2 = '_iw=imagenet'
-                elif model == 'Supervised-random':
-                    filter1 = 'Supervised'
-                    filter2 = '_iw=random'
+                # Get a list of all directories.
+                files = os.listdir(curr_path)
+                filtered_files = sorted([f for f in files if tla in f])
 
                 # Filter current files.
-                curr_mean = [f for f in mean_files
+                curr_mean_file = [f for f in filtered_files
                              if ('pp_mean_' in f and filter1 in f and filter2 in f)][0]
-                curr_std = [f for f in std_files
+                curr_std_file = [f for f in filtered_files
                             if ('pp_std_' in f and filter1 in f and filter2 in f)][0]
 
-                # Create final filenames.
-                curr_mean_filename = os.path.join(curr_path, curr_mean)
-                curr_std_filename = os.path.join(curr_path, curr_std)
-                if args.verbose:
-                    print(f"\n{'- File:'.ljust(9)}{curr_mean_filename}")
-                    print(f"{'- File:'.ljust(9)}{curr_std_filename}")
+                # Append filenames.
+                mean_files.append(os.path.join(curr_path, curr_mean_file))
+                std_files.append(os.path.join(curr_path, curr_std_file))
 
-                # Read the CSV file into a pandas DataFrame.
-                res_mean_last_epoch = pd.read_csv(curr_mean_filename).iloc[-1, :]['f1_macro']
-                res_std_last_epoch = pd.read_csv(curr_std_filename).iloc[-1, :]['f1_macro']
+                # Append mean and std values.
+                res_mean_last_epoch = pd.read_csv(os.path.join(curr_path, curr_mean_file)).iloc[-1, :]['f1_macro']
+                mean_values.append(res_mean_last_epoch)
+                res_std_last_epoch = pd.read_csv(os.path.join(curr_path, curr_std_file)).iloc[-1, :]['f1_macro']
+                std_values.append(res_std_last_epoch)
 
-                # Append results.
-                # results[model].append((res_mean_last_epoch, res_std_last_epoch))
-                print(res_mean_last_epoch)
-                print(res_std_last_epoch)
+            # Show information.
+            print('Target files:')
+            for mfile, sfile, mvalue, svalue in zip(mean_files, std_files, mean_values, std_values):
+                print(mfile, '-->', mvalue)
+                print(sfile, '-->', svalue)
+
+
+
+
+
+
+                # # Filter current files.
+                # curr_mean = [f for f in mean_files
+                #              if ('pp_mean_' in f and filter1 in f and filter2 in f)][0]
+                # curr_std = [f for f in std_files
+                #             if ('pp_std_' in f and filter1 in f and filter2 in f)][0]
+
+                # # Create final filenames.
+                # curr_mean_filename = os.path.join(curr_path, curr_mean)
+                # curr_std_filename = os.path.join(curr_path, curr_std)
+                # if args.verbose:
+                #     print(f"\n{'- File:'.ljust(9)}{curr_mean_filename}")
+                #     print(f"{'- File:'.ljust(9)}{curr_std_filename}")
+
+                # # Read the CSV file into a pandas DataFrame.
+                # res_mean_last_epoch = pd.read_csv(curr_mean_filename).iloc[-1, :]['f1_macro']
+                # res_std_last_epoch = pd.read_csv(curr_std_filename).iloc[-1, :]['f1_macro']
+
+                # # Append results.
+                # # results[model].append((res_mean_last_epoch, res_std_last_epoch))
+                # print(res_mean_last_epoch)
+                # print(res_std_last_epoch)
 
 
     return 0
