e82b5974f2bf8da3d9433c868570b9cd48594c80
sfandres94 <andres.sanchez1994@gmail.com>
2024-01-06 22:51:23 +0100

    Increase number of exps

--- finetuning_run_raytune.sh
+++ finetuning_run_raytune.sh
@@ -3,11 +3,11 @@
 # Define the variables.
 backbones=("resnet18")                                      # "resnet18" "resnet50"
 train_rates=("05" "10")                                     # "0.01" "0.05" "0.1" "0.25" "0.5" "1.0" "25" "50" "75" "100"
-downstream=("multiclass")                                   # "multiclass" "multilabel"
-models=("Supervised")                                       # "Supervised" "BarlowTwins" "MoCov2" "SimCLR" "SimCLRv2" "SimSiam"
-ini_weights=("random")                                      # "random" "imagenet"
+downstream=("multiclass" "multilabel")                      # "multiclass" "multilabel"
+models=("Supervised" "BarlowTwins")                         # "Supervised" "BarlowTwins" "MoCov2" "SimCLR" "SimCLRv2" "SimSiam"
+ini_weights=("random" "imagenet")                           # "random" "imagenet"
 balanced_dataset=(" ")                                      # " " "-bd" (" " for Ray Tune)
-transfer_learning=("LP")                                    # "LP" "FT" "LP+FT" (only the first two for Ray Tune)
+transfer_learning=("LP" "FT")                               # "LP" "FT" "LP+FT" (only the first two for Ray Tune)
 seeds=("42")                                                # Random: "42" (only this for Ray Tune) "5" "97"
 epochs=100                                                  # 15 for Ray Tune; otherwise 100
 learning_rate=0.01                                          # Not used for Ray Tune or --load_best_hyperparameters
@@ -35,16 +35,19 @@ for b in "${backbones[@]}"; do
                         if [ "$m" = "Supervised" ]; then
                             for iw in "${ini_weights[@]}"; do
                                 command_arg="torchrun finetuning.py $m $d -bn $b -tr $tr -e $epochs -lr $learning_rate -se $save_every -bs $batch_size -nw $num_workers -iw $iw -tl $tl -s $s $more_options"
-                                raytune_args="--exp-name ${tr}_${d}_${tl}_${s}_${m}_${iw} --partition volta --num-nodes 1 --node aap04 --num-gpus 4 --load-env \"ssl-bsu-conda\" --command \"$command_arg\"" # --node aap04 
+                                raytune_args="--exp-name ${tr}_${d}_${tl}_${s}_${m}_${iw} --partition volta --num-nodes 1 --num-gpus 4 --load-env \"ssl-bsu-conda\" --command \"$command_arg\"" # --node aap04 
                                 final_command="python slurm-launch.py $raytune_args"
                                 echo $final_command
                                 eval $final_command
                             done
-                        # else
-                        #     for bd in "${balanced_dataset[@]}"; do
-                        #         command="torchrun finetuning.py $m $d -bn $b -tr $tr -e $epochs -lr $learning_rate -se $save_every -bs $batch_size -nw $num_workers -iw random -tl $tl -s $s $bd $more_options"
-                        #         echo $command; $command # >> out_finetuning_$d_$m.out
-                        #     done
+                        else
+                            for bd in "${balanced_dataset[@]}"; do
+                                command_arg="torchrun finetuning.py $m $d -bn $b -tr $tr -e $epochs -lr $learning_rate -se $save_every -bs $batch_size -nw $num_workers -iw random -tl $tl -s $s $bd $more_options"
+                                raytune_args="--exp-name ${tr}_${d}_${tl}_${s}_${m}_${iw} --partition volta --num-nodes 1 -num-gpus 4 --load-env \"ssl-bsu-conda\" --command \"$command_arg\"" # --node aap04 
+                                final_command="python slurm-launch.py $raytune_args"
+                                echo $final_command
+                                eval $final_command
+                            done
                         fi
                     done
                 done
